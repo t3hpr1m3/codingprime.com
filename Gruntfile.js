d0bd82ef3c4636c6f59cd6f11cd7a0578b308f80
Josh Williams <vmizzle@gmail.com>
2014-12-01 22:07:26 +0000

    Fixed single post template.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,15 @@ module.exports = function(grunt) {
       build: {
         options: {
           metadata: {
-            title: 'CodingPrime',
-            githubUsername: 't3hpr1m3',
-            twitterUsername: 't3hpr1m3',
-            description: 'Write an awesome description for your new site here. You \
-  can edit this line in _config.yml. It will appear in your document \
-  head meta (for Google search results) and in your feed.xml site \
-  description.',
+            site: {
+              title: 'CodingPrime',
+              githubUsername: 't3hpr1m3',
+              twitterUsername: 't3hpr1m3',
+              description: 'Write an awesome description for your new site here. You \
+    can edit this line in _config.yml. It will appear in your document \
+    head meta (for Google search results) and in your feed.xml site \
+  description.'
+            },
             moment: require('moment')
           },
           plugins: {
